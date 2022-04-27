f75cae565cfac25373ecf5932687db26dd5e292d
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-04-27 09:18:59 -0400

    commit tree a5c19fb1658d6d2b027c675c5899f2a92b6acdbb

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+const num1 = 15.5;
+const num2 = 4;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 62) + 1;
+const num3 = 12
+const num4 = 8
+const mod = num3 % num4;
+const max = Math.max(2, 8, 20)
+//function multiply(num1, num2) {
+//    return num1 * num2 === 62;
+//}
